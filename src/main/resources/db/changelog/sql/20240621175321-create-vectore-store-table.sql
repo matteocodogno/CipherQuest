f579e8d64b9ae7365494415e046b64c815c2bfd5
Matteo Codogno <matteo.codogno@welld.ch>
2024-06-24 14:58:23 +0200

    feat: add pgvector as vector store

---
+++ src/main/resources/db/changelog/sql/20240621175321-create-vectore-store-table.sql
@@ -0,0 +1,20 @@
+--changeset matteo_codogno:20240621175321-1 context:generate_skip
+--comment: Install extensions
+
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS hstore;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+--changeset matteo_codogno:20240621175321-2
+--comment: Create the vector_store table
+
+CREATE TABLE IF NOT EXISTS vector_store
+(
+    id        uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    content   text,
+    metadata  json,
+    embedding vector(1536) -- 1536 is the default embedding dimension
+);
+
+CREATE INDEX ON vector_store USING HNSW (embedding vector_cosine_ops);
+
