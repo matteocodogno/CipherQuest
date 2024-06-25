560b187db7ca912acec18b343d3ba3a4e4f782b2
Matteo Codogno <matteo.codogno@welld.ch>
2024-06-25 17:59:43 +0200

    feat(rag): wip on load document

---
+++ src/main/kotlin/code/nebula/cipherquest/controller/RagController.kt
@@ -0,0 +1,42 @@
+package code.nebula.cipherquest.controller
+
+import code.nebula.cipherquest.service.VectorStoreService
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.ai.reader.TextReader
+import org.springframework.core.io.ResourceLoader
+import org.springframework.util.ResourceUtils
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.nio.charset.Charset
+
+private val logger = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/rag")
+class RagController(
+    val vectorStoreService: VectorStoreService,
+    val resourceLoader: ResourceLoader,
+) {
+
+    @PostMapping("/load")
+    fun load() {
+        ResourceUtils.getFile("classpath:documents/")
+            .listFiles()
+            ?.mapTo(ArrayList()) { file ->
+                TextReader(resourceLoader.getResource("classpath:documents/${file.name}"))
+                    .let { reader ->
+                        reader.customMetadata.putAll(mapOf("level" to file.name.split("-")[1]))
+                        reader.charset = Charset.defaultCharset()
+                        reader.get()
+                    }
+            }
+            ?.forEach { documents ->
+                logger.info { "Loading ${documents.size} documents" }
+                vectorStoreService.loadDocument(documents)
+            }
+
+//        vectorStoreService.loadDocument(documents)
+    }
+
+}
---
+++ src/main/kotlin/code/nebula/cipherquest/service/VectorStoreService.kt
@@ -0,0 +1,13 @@
+package code.nebula.cipherquest.service
+
+import org.springframework.ai.document.Document
+import org.springframework.ai.transformer.splitter.TokenTextSplitter
+import org.springframework.ai.vectorstore.VectorStore
+import org.springframework.stereotype.Service
+
+@Service
+class VectorStoreService(
+    val vectorStore: VectorStore
+) {
+    fun loadDocument(docs: List<Document>) = vectorStore.add(TokenTextSplitter().split(docs))
+}
