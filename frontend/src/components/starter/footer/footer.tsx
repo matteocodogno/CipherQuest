6f579c59f3f90d9d54d0a2a2c221c70b44f7f6ad
Matteo Codogno <matteo.codogno@welld.ch>
2024-07-12 23:12:03 +0200

    feat(ui): change footer link

--- frontend/src/components/starter/footer/footer.tsx
+++ frontend/src/components/starter/footer/footer.tsx
@@ -8,7 +8,7 @@ export default component$(() => {
   return (
     <footer class="mt-auto">
       <div class="flex justify-center">
-        <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
+        <a href="https://github.com/matteocodogno/CipherQuest" target="_blank" class={styles.anchor}>
           <span>Made with ♡ by Matteo Codogno and Andrea Rubino</span>
           <span class={styles.spacer}>|</span>
           <span>{serverTime.value.date}</span>
