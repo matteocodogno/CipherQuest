dfe3ba4f96fe7384150f44f74dc2076b7a04d9ec
Matteo Codogno <matteo.codogno@welld.ch>
2024-10-17 12:05:30 +0200

    feat: upgrade to spring-ai M3

--- backend/src/main/kotlin/code/nebula/cipherquest/configuration/ChatClientConfiguration.kt
+++ backend/src/main/kotlin/code/nebula/cipherquest/configuration/ChatClientConfiguration.kt
@@ -2,10 +2,10 @@ package code.nebula.cipherquest.configuration
 
 import code.nebula.cipherquest.advisor.LevelUpAdvisor
 import code.nebula.cipherquest.advisor.LoggingAdvisor
-import code.nebula.cipherquest.advisor.PgVectorStoreChatMemoryAdvisor
 import code.nebula.cipherquest.service.UserLevelService
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor
+import org.springframework.ai.chat.client.advisor.VectorStoreChatMemoryAdvisor
 import org.springframework.ai.chat.model.ChatModel
 import org.springframework.ai.vectorstore.SearchRequest
 import org.springframework.ai.vectorstore.VectorStore
@@ -19,6 +19,8 @@ class ChatClientConfiguration(
     private val chatModel: ChatModel,
     private val vectorStore: VectorStore,
     private val userLevelService: UserLevelService,
+    @Value("\${overmind.ai.chat.history.max-size}")
+    private val chatHistoryWindowSize: Int,
 ) {
     @Value("classpath:/prompts/system-message.st")
     private lateinit var systemMessageResource: Resource
@@ -38,7 +40,7 @@ class ChatClientConfiguration(
         return builder
             .defaultSystem(systemMessageResource)
             .defaultAdvisors(
-                PgVectorStoreChatMemoryAdvisor(vectorStore, memorySystemText),
+                VectorStoreChatMemoryAdvisor(vectorStore, memorySystemText, chatHistoryWindowSize),
                 QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults().withTopK(1), ragSystemText),
                 LevelUpAdvisor(vectorStore, userLevelService),
                 LoggingAdvisor(),
