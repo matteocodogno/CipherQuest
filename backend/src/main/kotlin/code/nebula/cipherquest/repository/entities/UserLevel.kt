509e13676af18583d4bd2b2675c5ce2a25052958
edoparo <edo.paronzini@gmail.com>
2024-10-29 11:51:58 +0100

    feat(backend): keep the score updated through the game (#43)

--- backend/src/main/kotlin/code/nebula/cipherquest/repository/entities/UserLevel.kt
+++ backend/src/main/kotlin/code/nebula/cipherquest/repository/entities/UserLevel.kt
@@ -2,6 +2,8 @@ package code.nebula.cipherquest.repository.entities
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import java.time.OffsetDateTime
 
 @Entity
@@ -11,5 +13,10 @@ data class UserLevel(
     var level: Int,
     var username: String,
     var coins: Int = 25,
+    @CreationTimestamp
+    var createdAt: OffsetDateTime = OffsetDateTime.now(),
+    @UpdateTimestamp
+    var updatedAt: OffsetDateTime = OffsetDateTime.now(),
     var terminatedAt: OffsetDateTime? = null,
+    var score: Long = 0,
 )
