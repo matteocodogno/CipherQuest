22562facd4fbd325598adf6d5b3f719ac8cacf6e
Andrea Rubino <24411828+rubin0@users.noreply.github.com>
2024-11-07 15:34:00 +0100

    feat: add intro message to message history

--- backend/src/main/kotlin/code/nebula/cipherquest/service/VectorStoreService.kt
+++ backend/src/main/kotlin/code/nebula/cipherquest/service/VectorStoreService.kt
@@ -1,15 +1,22 @@
 package code.nebula.cipherquest.service
 
+import code.nebula.cipherquest.models.dto.Message
 import code.nebula.cipherquest.repository.VectorStoreRepository
+import org.springframework.ai.chat.messages.MessageType
+import org.springframework.ai.chat.prompt.PromptTemplate
 import org.springframework.ai.document.Document
 import org.springframework.ai.transformer.splitter.TokenTextSplitter
 import org.springframework.ai.vectorstore.VectorStore
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 
 @Service
 class VectorStoreService(
     val vectorStore: VectorStore,
     val vectorStoreRepository: VectorStoreRepository,
+    @Value("classpath:/prompts/intro-message.st")
+    private var introMessageResource: Resource,
 ) {
     fun loadDocument(docs: List<Document>) = vectorStore.add(TokenTextSplitter().split(docs))
 
@@ -17,5 +24,26 @@ class VectorStoreService(
 
     fun existsDocumentWithFileName(filename: String) = vectorStoreRepository.existsDocumentWithFileName(filename)
 
-    fun getMessageHistoryByUserId(userId: String) = vectorStoreRepository.getMessageHistoryByUserId(userId)
+    fun getMessageHistoryByUserId(userId: String): List<Message> =
+        vectorStoreRepository.getMessageHistoryByUserId(userId).ifEmpty {
+            val introMessage = introMessageResource.getContentAsString(Charsets.UTF_8)
+            val introMessageInterpolated =
+                PromptTemplate(introMessage)
+                    .create(mapOf("userId" to userId))
+                    .contents
+
+            vectorStore.add(
+                listOf(
+                    Document(
+                        introMessageInterpolated,
+                        mapOf(
+                            "conversationId" to userId,
+                            "messageType" to MessageType.ASSISTANT.name,
+                        ) as Map<String, Any>,
+                    ),
+                ),
+            )
+
+            vectorStoreRepository.getMessageHistoryByUserId(userId)
+        }
 }
