68b58013e3c944b530722cde7a25e356b08e8b7b
Simone Cimoli <simone.cimoli@welld.ch>
2024-11-11 17:54:08 +0100

    refactor: create welcome folder

---
+++ frontend/src/components/welcome/constants.ts
@@ -0,0 +1,29 @@
+export const AccordionMenu = [
+  {
+    title: 'Mission goal',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+    Repudiandae, mollitia id reprehenderit a ab odit!`,
+  },
+  {
+    title: 'Mission rules',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+    Repudiandae, mollitia id reprehenderit a ab odit!`,
+  },
+  {
+    title: 'Other mission infos',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+    Repudiandae, mollitia id reprehenderit a ab odit!`,
+  },
+];
--- frontend/src/pages/game/rules.tsx
+++ frontend/src/pages/game/rules.tsx
@@ -1,10 +1,12 @@
 import { Helmet } from 'react-helmet-async';
 import { Metadata } from '@/types/metadata';
 import { ReactElement } from 'react';
-import { RulesView } from '@/components/chat/rules-view.tsx';
+import { RulesView } from '@/components/welcome/rules-view';
 import { config } from '@/config.ts';
 
-const metadata = { title: `Rules | Overmind | ${config.site.name}` } satisfies Metadata;
+const metadata = {
+  title: `Rules | Overmind | ${config.site.name}`,
+} satisfies Metadata;
 
 export const Page = (): ReactElement => (
   <>
