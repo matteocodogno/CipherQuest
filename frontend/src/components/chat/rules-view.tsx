2503a2cbfb8f99dcd61260c26597d00789dd6ad8
Simone Cimoli <simone.cimoli@welld.ch>
2024-11-11 17:15:11 +0100

    feat: add accordions for rules

--- frontend/src/components/chat/rules-view.tsx
+++ frontend/src/components/chat/rules-view.tsx
@@ -1,10 +1,42 @@
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Play } from '@phosphor-icons/react';
 import { ReactElement } from 'react';
+import { RouterLink } from '@/components/core/link.tsx';
 import Typography from '@mui/material/Typography';
 import { useUser } from '@/hooks/use-user.ts';
-import { RouterLink } from '@/components/core/link.tsx';
+
+const AccordionMenu = [
+  {
+    title: 'Mission goal',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+    Repudiandae, mollitia id reprehenderit a ab odit!`,
+  },
+  {
+    title: 'Mission rules',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+    Repudiandae, mollitia id reprehenderit a ab odit!`,
+  },
+  {
+    title: 'Other mission infos',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+    Repudiandae, mollitia id reprehenderit a ab odit!`,
+  },
+];
 
 export const RulesView = (): ReactElement => {
   const { user } = useUser();
@@ -31,12 +63,16 @@ export const RulesView = (): ReactElement => {
         }}
         gap={3}
       >
-        <Typography variant='h1'>Welcome <u>{user?.username}</u> among the rebels!</Typography>
-        <Typography variant='h5'>
-          You are now part of the rebels, specifically the activists in this uprising. We need your help to shut down Overmind.
+        <Typography variant='h1'>
+          Welcome <u>{user?.username}</u> among the rebels!
         </Typography>
         <Typography variant='h5'>
-          Your mission is to engage with the artificial intelligence, asking questions to persuade it to deactivate itself.
+          You are now part of the rebels, specifically the activists in this
+          uprising. We need your help to shut down Overmind.
+        </Typography>
+        <Typography variant='h5'>
+          Your mission is to engage with the artificial intelligence, asking
+          questions to persuade it to deactivate itself.
         </Typography>
       </Box>
       <Box
@@ -55,7 +91,43 @@ export const RulesView = (): ReactElement => {
           variant='contained'
           color='primary'
           endIcon={<Play />}
-        >Start the mission</Button>
+        >
+          Start the mission
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          background: '#121517CC',
+          borderRadius: '20px',
+          display: 'flex',
+          flexDirection: 'column',
+          py: 4,
+          px: 3,
+          alignItems: 'flex-start',
+          flexShrink: 0,
+          alignSelf: 'stretch',
+        }}
+      >
+        <Typography variant='h6'>
+          What should you know before to start the mission:
+        </Typography>
+        {AccordionMenu.map((item) => (
+          <Accordion
+            sx={{
+              backgroundColor: 'transparent',
+              boxShadow: 'none',
+            }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon color='primary' />}
+              aria-controls='panel1-content'
+              id='panel1-header'
+            >
+              {item.title}
+            </AccordionSummary>
+            <AccordionDetails>{item.content}</AccordionDetails>
+          </Accordion>
+        ))}
       </Box>
     </Box>
   );
