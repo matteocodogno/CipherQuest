342b860e444cdec9cb8780a0a7c860ea7f3fd807
Simone Cimoli <simone.cimoli@welld.ch>
2024-12-16 16:33:59 +0100

    feat: create custom dialog

---
+++ frontend/src/components/chat/dialog/source-dialog.tsx
@@ -0,0 +1,66 @@
+import CloseIcon from '@mui/icons-material/Close';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+interface ContentDialogProps {
+  content: string;
+  showDialog: boolean;
+  closeDialog: () => void;
+}
+
+const SourceDialog = ({ showDialog, closeDialog }: ContentDialogProps) => {
+  return (
+    <BootstrapDialog
+      onClose={closeDialog}
+      aria-labelledby='customized-dialog-title'
+      open={showDialog}
+    >
+      <DialogTitle sx={{ m: 0, p: 2 }} id='customized-dialog-title'>
+        Source
+      </DialogTitle>
+      <IconButton
+        aria-label='close'
+        onClick={closeDialog}
+        sx={(theme) => ({
+          position: 'absolute',
+          right: 8,
+          top: 8,
+          color: theme.palette.grey[500],
+        })}
+      >
+        <CloseIcon />
+      </IconButton>
+      <DialogContent>
+        <Typography gutterBottom>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </Typography>
+        <Typography gutterBottom>
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+          Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+        </Typography>
+        <Typography gutterBottom>
+          Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+          magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+          ullamcorper nulla non metus auctor fringilla.
+        </Typography>
+      </DialogContent>
+    </BootstrapDialog>
+  );
+};
+
+export default SourceDialog;
