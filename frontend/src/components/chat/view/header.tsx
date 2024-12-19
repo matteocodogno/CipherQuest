4ec3d506d394da981480c1fe8208cc51644927fb
Simone Cimoli <simone.cimoli@welld.ch>
2024-12-19 11:49:33 +0100

    feat: show logout and mission rules buttons

--- frontend/src/components/chat/view/header.tsx
+++ frontend/src/components/chat/view/header.tsx
@@ -1,6 +1,8 @@
+import { Button, Divider } from '@mui/material';
 import Box from '@mui/material/Box';
 import { DynamicLogo } from '@/components/core/logo.tsx';
 import { ReactElement } from 'react';
+import { Stack } from '@mui/system';
 
 export const Header = (): ReactElement => (
   <Box
@@ -13,5 +15,27 @@ export const Header = (): ReactElement => (
     py={1}
   >
     <DynamicLogo width={164} height={46} />
+    <Stack
+      flex={1}
+      direction={'row'}
+      justifyContent='end'
+      alignItems='end'
+      gap={1}
+    >
+      <Button
+        variant='text'
+        endIcon={<Box component='img' src={'/assets/mission-rules.svg'} />}
+      >
+        Mission rules
+      </Button>
+      <Divider orientation='vertical' variant='middle' flexItem />
+      <Button
+        variant='text'
+        endIcon={<Box component='img' src={'/assets/logout.svg'} />}
+        sx={{ color: 'var(--mui-palette-error-dark)' }}
+      >
+        End mission
+      </Button>
+    </Stack>
   </Box>
 );
