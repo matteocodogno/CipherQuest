b7e0d898a3a955fa201e1d8d9b84266470b457a5
Simone Cimoli <simone.cimoli@welld.ch>
2024-12-19 15:45:06 +0100

    feat: show buttons only when the game has started

--- frontend/src/components/chat/view/header.tsx
+++ frontend/src/components/chat/view/header.tsx
@@ -3,39 +3,46 @@ import Box from '@mui/material/Box';
 import { DynamicLogo } from '@/components/core/logo.tsx';
 import { ReactElement } from 'react';
 import { Stack } from '@mui/system';
+import { useUser } from '@/hooks/use-user';
 
-export const Header = (): ReactElement => (
-  <Box
-    style={{
-      display: 'flex',
-      alignItems: 'center',
-      alignSelf: 'stretch',
-    }}
-    px={3}
-    py={1}
-  >
-    <DynamicLogo width={164} height={46} />
-    <Stack
-      flex={1}
-      direction={'row'}
-      justifyContent='end'
-      alignItems='end'
-      gap={1}
+export const Header = (): ReactElement => {
+  const { user } = useUser();
+
+  return (
+    <Box
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+      }}
+      px={3}
+      py={1}
     >
-      <Button
-        variant='text'
-        endIcon={<Box component='img' src={'/assets/mission-rules.svg'} />}
-      >
-        Mission rules
-      </Button>
-      <Divider orientation='vertical' variant='middle' flexItem />
-      <Button
-        variant='text'
-        endIcon={<Box component='img' src={'/assets/logout.svg'} />}
-        sx={{ color: 'var(--mui-palette-error-dark)' }}
-      >
-        End mission
-      </Button>
-    </Stack>
-  </Box>
-);
+      <DynamicLogo width={164} height={46} />
+      {user?.startedAt && (
+        <Stack
+          flex={1}
+          direction={'row'}
+          justifyContent='end'
+          alignItems='end'
+          gap={1}
+        >
+          <Button
+            variant='text'
+            endIcon={<Box component='img' src={'/assets/mission-rules.svg'} />}
+          >
+            Mission rules
+          </Button>
+          <Divider orientation='vertical' variant='middle' flexItem />
+          <Button
+            variant='text'
+            endIcon={<Box component='img' src={'/assets/logout.svg'} />}
+            sx={{ color: 'var(--mui-palette-error-dark)' }}
+          >
+            End mission
+          </Button>
+        </Stack>
+      )}
+    </Box>
+  );
+};
