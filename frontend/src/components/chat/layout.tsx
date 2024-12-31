d4d0a6d8d15f3a33d81f191754931f6cb7828e4c
Matteo Codogno <matteo.codogno@welld.ch>
2024-12-31 10:30:05 +0100

    refactor(fe): wrap the chat page with chat provider

--- frontend/src/components/chat/layout.tsx
+++ frontend/src/components/chat/layout.tsx
@@ -1,12 +1,9 @@
 import { ReactElement, ReactNode } from 'react';
 import { AuthGuard } from '@/components/auth/auth-guard.tsx';
 import Box from '@mui/material/Box';
-import { ChatProvider } from '@/contexts/chat/chat-provider.tsx';
 import { Header } from './header.tsx';
 import { Stack } from '@mui/system';
-import useGetChatHistory from '@/api/chat/use-get-chat-history';
 import { usePathname } from '@/hooks/use-pathname.ts';
-import { useUser } from '@/hooks/use-user';
 
 type LayoutProps = {
   children: ReactNode;
@@ -20,43 +17,37 @@ const backgroundMap: Record<string, string> = {
 export function Layout({ children }: LayoutProps): ReactElement {
   const pathname = usePathname();
   const background = backgroundMap[pathname] ?? '/assets/background.jpeg';
-  const { user } = useUser();
-  const { messages } = useGetChatHistory({
-    user,
-  });
 
   return (
     <AuthGuard>
-      <ChatProvider messages={messages}>
-        <Box
-          style={{
-            height: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundImage: `linear-gradient(0deg, rgba(9, 10, 11, 0.9) 0%, rgba(9, 10, 11, 0.9) 100%), url("${background}")`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            overflow: 'hidden',
+      <Box
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundImage: `linear-gradient(0deg, rgba(9, 10, 11, 0.9) 0%, rgba(9, 10, 11, 0.9) 100%), url("${background}")`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          overflow: 'hidden',
+        }}
+      >
+        <Header />
+        <Stack
+          sx={{
+            width: '70%',
+            minWidth: '600px',
+            paddingX: 7,
+            height: 'calc(100vh - 62px)',
+            overflowY: 'scroll',
           }}
+          flexDirection='column'
+          alignItems={'center'}
         >
-          <Header />
-          <Stack
-            sx={{
-              width: '70%',
-              minWidth: '600px',
-              paddingX: 7,
-              height: 'calc(100vh - 62px)',
-              overflowY: 'scroll',
-            }}
-            flexDirection='column'
-            alignItems={'center'}
-          >
-            {children}
-          </Stack>
-        </Box>
-      </ChatProvider>
+          {children}
+        </Stack>
+      </Box>
     </AuthGuard>
   );
 }
--- frontend/src/pages/game/chat.tsx
+++ frontend/src/pages/game/chat.tsx
@@ -1,20 +1,32 @@
+import { ChatProvider } from '@/contexts/chat/chat-provider.tsx';
 import { ChatView } from '@/components/chat/view/chat-view';
 import ComposeView from '@/components/chat/view/compose-view.tsx';
 import { Helmet } from 'react-helmet-async';
 import { Metadata } from '@/types/metadata';
 import { ReactElement } from 'react';
 import { config } from '@/config.ts';
+import useGetChatHistory from '@/api/chat/use-get-chat-history.ts';
+import { useUser } from '@/hooks/use-user.ts';
 
 const metadata = {
   title: `Game | Overmind | ${config.site.name}`,
 } satisfies Metadata;
 
-export const Page = (): ReactElement => (
-  <>
-    <Helmet>
-      <title>{metadata.title}</title>
-    </Helmet>
-    <ChatView />
-    <ComposeView />
-  </>
-);
+export const Page = (): ReactElement => {
+  const { user } = useUser();
+  const { messages } = useGetChatHistory({
+    user,
+  });
+
+  return (
+    <>
+      <Helmet>
+        <title>{metadata.title}</title>
+      </Helmet>
+      <ChatProvider messages={messages}>
+        <ChatView />
+        <ComposeView />
+      </ChatProvider>
+    </>
+  );
+};
