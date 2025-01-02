0cc346a9bf3ea2aa363f34feb401c82861954b31
Andrea Rubino <24411828+rubin0@users.noreply.github.com>
2025-01-02 11:56:17 +0100

    feat: add zod userStatus enum validation

--- frontend/src/api/chat/types.ts
+++ frontend/src/api/chat/types.ts
@@ -2,3 +2,10 @@ export enum SenderType {
   USER = 'USER',
   OVERMIND = 'ASSISTANT',
 }
+
+export enum UserStatus {
+  IN_PROGRESS = 'IN_PROGRESS',
+  WIN = 'WIN',
+  GAME_OVER = 'GAME_OVER',
+  CHEATED = 'CHEATED',
+}
