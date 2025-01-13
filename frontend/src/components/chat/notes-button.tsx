0e7ac7612b268da06b439026d32110fa8095c636
Simone Cimoli <simone.cimoli@welld.ch>
2025-01-13 14:39:07 +0100

    fix: notes button position on mobile

--- frontend/src/components/chat/notes-button.tsx
+++ frontend/src/components/chat/notes-button.tsx
@@ -1,28 +1,32 @@
 import { Fab } from '@mui/material';
 import { Notepad } from '@phosphor-icons/react';
 import { NotesButtonVariant } from './constants';
+import useIsMobile from '@/hooks/use-is-mobile';
 
 interface NotesButtonProps {
   handleClick: () => void;
   variant?: NotesButtonVariant;
 }
 
-const NotesButton = ({ handleClick, variant }: NotesButtonProps) => (
-  <Fab
-    variant={variant}
-    color='secondary'
-    size='large'
-    sx={{
-      position: 'absolute',
-      right: 32,
-      bottom: 92,
-      color: 'var(--mui-palette-primary-700)',
-    }}
-    onClick={handleClick}
-  >
-    {variant === 'extended' ? 'Block notes' : ''}
-    <Notepad size={24} />
-  </Fab>
-);
+const NotesButton = ({ handleClick, variant }: NotesButtonProps) => {
+  const isMobile = useIsMobile();
+  return (
+    <Fab
+      variant={variant}
+      color='secondary'
+      size='large'
+      sx={{
+        position: 'absolute',
+        right: isMobile ? 24 : 32,
+        bottom: isMobile ? 48 : 92,
+        color: 'var(--mui-palette-primary-700)',
+      }}
+      onClick={handleClick}
+    >
+      {variant === 'extended' ? 'Block notes' : ''}
+      <Notepad size={24} />
+    </Fab>
+  );
+};
 
 export default NotesButton;
