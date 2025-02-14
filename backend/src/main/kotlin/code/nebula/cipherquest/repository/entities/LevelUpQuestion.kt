2eaab06b3009f63964a2746d59910a5c5e99aa26
Sara Mangialavori <sara.mangialavori@welld.ch>
2025-02-14 17:03:45 +0100

    feat: add endpoints to add level up and protected questions

--- backend/src/main/kotlin/code/nebula/cipherquest/repository/entities/LevelUpQuestion.kt
+++ backend/src/main/kotlin/code/nebula/cipherquest/repository/entities/LevelUpQuestion.kt
@@ -1,13 +1,17 @@
 package code.nebula.cipherquest.repository.entities
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import java.util.*
 
 @Entity
 data class LevelUpQuestion(
     @Id
-    var id: String,
-    var level: Int,
-    var question: String,
-    var storyName: String,
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+    val level: Int,
+    val question: String,
+    val storyName: String,
 )
