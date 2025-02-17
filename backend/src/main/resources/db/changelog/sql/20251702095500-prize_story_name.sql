5e4fa4bfa036c1a0f84fbd175ff87ca50a2e1d9e
Andrea Rubino <24411828+rubin0@users.noreply.github.com>
2025-02-17 10:11:16 +0100

    feat: add prize_name column to prize table

---
+++ backend/src/main/resources/db/changelog/sql/20251702095500-prize_story_name.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset andrea.rubino:20251702095500-1
+--comment: Add column story_name
+--preconditions onFail:MARK_RAN onError:HALT
+--precondition-sql-check expectedResult:0 select count(*) from information_schema.columns where table_name='prize' and column_name='story_name';
+
+ALTER TABLE prize
+    ADD COLUMN story_name TEXT NULL;
+
