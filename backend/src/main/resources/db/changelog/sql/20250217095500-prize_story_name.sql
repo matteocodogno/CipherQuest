4a32a6258d0f8d33823c24736f5953c9ff5d00c6
Andrea Rubino <24411828+rubin0@users.noreply.github.com>
2025-02-17 16:27:10 +0100

    fix: migration file name

--- backend/src/main/resources/db/changelog/sql/20250217095500-prize_story_name.sql
+++ backend/src/main/resources/db/changelog/sql/20250217095500-prize_story_name.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset andrea.rubino:20251702095500-1
+--changeset andrea.rubino:20250217095500-1
 --comment: Add column story_name
 --preconditions onFail:MARK_RAN onError:HALT
 --precondition-sql-check expectedResult:0 select count(*) from information_schema.columns where table_name='prize' and column_name='story_name';
