c3e3b44f5d2ec26752410445187f9676b9ce3102
Sara Mangialavori <sara.mangialavori@welld.ch>
2025-02-20 12:14:43 +0100

    refactor(backend): require row mapper implementation in question repository subclasses

--- backend/src/main/kotlin/code/nebula/cipherquest/repository/LevelUpQuestionRepository.kt
+++ backend/src/main/kotlin/code/nebula/cipherquest/repository/LevelUpQuestionRepository.kt
@@ -4,8 +4,8 @@ import code.nebula.cipherquest.models.requests.LevelUpQuestionRequest
 import code.nebula.cipherquest.repository.entities.LevelUpQuestion
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
-import java.sql.ResultSet
 import java.util.UUID
 
 @Repository
@@ -19,11 +19,13 @@ class LevelUpQuestionRepository(
     ) {
     override val tableName = "level_up_question"
 
-    override fun buildQuestion(rs: ResultSet) =
-        LevelUpQuestion(
-            id = UUID.fromString(rs.getString("id")),
-            level = rs.getInt("level"),
-            storyName = rs.getString("storyName"),
-            content = rs.getString("content"),
-        )
+    override val questionMapper =
+        RowMapper<LevelUpQuestion> { rs, _ ->
+            LevelUpQuestion(
+                id = UUID.fromString(rs.getString("id")),
+                content = rs.getString("content"),
+                storyName = rs.getString("storyName"),
+                level = rs.getInt("level"),
+            )
+        }
 }
