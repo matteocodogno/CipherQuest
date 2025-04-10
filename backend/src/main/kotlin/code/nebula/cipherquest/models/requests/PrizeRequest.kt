bd87c3b922696df94c862dcbc6b6cc5091716222
Sara Mangialavori <sara.mangialavori@welld.ch>
2025-04-10 16:40:14 +0200

    refactor(prize): add prizes on same date

--- backend/src/main/kotlin/code/nebula/cipherquest/models/requests/PrizeRequest.kt
+++ backend/src/main/kotlin/code/nebula/cipherquest/models/requests/PrizeRequest.kt
@@ -2,8 +2,12 @@ package code.nebula.cipherquest.models.requests
 
 import java.time.LocalDate
 
-data class PrizeRequest(
+data class Prize(
     val name: String,
     val position: Int,
+)
+
+data class PrizeRequest(
     val date: LocalDate = LocalDate.now(),
+    val prizes: List<Prize>,
 )
