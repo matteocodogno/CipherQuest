663d46c91ec8bea8154fdb97611f25c4d441386c
Sara Mangialavori <sara.mangialavori@welld.ch>
2025-04-28 14:37:11 +0200

    feat(config): add story name as value

--- backend/src/main/kotlin/code/nebula/cipherquest/configuration/properties/UniqueCodeMailProperties.kt
+++ backend/src/main/kotlin/code/nebula/cipherquest/configuration/properties/UniqueCodeMailProperties.kt
@@ -2,7 +2,7 @@ package code.nebula.cipherquest.configuration.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "overmind.mail.unique-code")
+@ConfigurationProperties(prefix = "cipher-quest.mail.unique-code")
 data class UniqueCodeMailProperties(
     val subject: String = "",
     val from: String = "",
