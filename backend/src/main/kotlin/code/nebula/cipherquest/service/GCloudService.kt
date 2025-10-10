ec629df459c331388c5643dc43826b5d7825b4b6
Giovanni Barbieri <giovanni.barbieri004@gmail.com>
2025-10-10 09:12:38 +0200

    feat: Created GCloud service to handle logic of Gcloud controller.
    feat: Ammended Request DTOs, giving them base values so when the json file is parsed, otherwise the object mapper provided by Jackson is unable to work.

---
+++ backend/src/main/kotlin/code/nebula/cipherquest/service/GCloudService.kt
@@ -0,0 +1,53 @@
+package code.nebula.cipherquest.service
+
+import code.nebula.cipherquest.configuration.properties.CloudStorageProperties
+import code.nebula.cipherquest.models.dto.GameDataFile
+import code.nebula.cipherquest.models.requests.FixedBotMessage
+import code.nebula.cipherquest.models.requests.FixedBotMessageRequest
+import code.nebula.cipherquest.models.requests.LevelUpQuestionRequest
+import code.nebula.cipherquest.models.requests.Prize
+import code.nebula.cipherquest.models.requests.PrizeRequest
+import code.nebula.cipherquest.models.requests.ProtectedQuestionRequest
+import code.nebula.cipherquest.repository.LevelUpQuestionRepository
+import code.nebula.cipherquest.repository.ProtectedQuestionRepository
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.cloud.storage.Storage
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+class GCloudService(
+    private val storage: Storage,
+    private val cloudStorageProperties: CloudStorageProperties,
+    private val prizeService: PrizeService,
+    private val fixedBotMessageService: FixedBotMessageService,
+    private val levelUpQuestionRepository: LevelUpQuestionRepository,
+    private val protectedQuestionRepository: ProtectedQuestionRepository,
+) {
+    @Transactional
+    fun loadContent(storyName: String): GameDataFile {
+        val blob =
+            storage[
+                cloudStorageProperties.storiesBucket.name,
+                "${cloudStorageProperties.storiesBucket.folder}/$storyName/loadContent.json",
+            ] ?: throw IllegalArgumentException("File not found in bucket ${cloudStorageProperties.storiesBucket.name}")
+
+        val mapper = ObjectMapper()
+
+        val json = String(blob.getContent())
+        val gameData = mapper.readValue(json, GameDataFile::class.java)
+
+        val levelUpQuestions = gameData.levelUpQuestions.map { LevelUpQuestionRequest(level = it.level, content = it.content) }
+        val protectedQuestions = gameData.protectedQuestions.map { ProtectedQuestionRequest(it.content) }
+        val fixedBotMessages =
+            FixedBotMessageRequest(messages = gameData.fixedBotMessages.map { FixedBotMessage(type = it.type, content = it.content) })
+        val prizeRequest = PrizeRequest(prizes = gameData.prizes.map { Prize(name = it.name, position = it.position) })
+
+        levelUpQuestionRepository.save(levelUpQuestions, storyName)
+        protectedQuestionRepository.save(protectedQuestions, storyName)
+        prizeService.addPrizes(prizeRequest, storyName)
+        fixedBotMessageService.addFixedBotMessages(fixedBotMessages, storyName)
+
+        return gameData
+    }
+}
