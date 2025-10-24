8ca42150fd1f91325da210777f20697161e3650f
Giovanni Barbieri <giovanni.barbieri004@gmail.com>
2025-10-24 12:20:23 +0200

    feat: amended the RecaptchaService to handle both v2 and v3 verification.

--- backend/src/main/kotlin/code/nebula/cipherquest/service/RecaptchaService.kt
+++ backend/src/main/kotlin/code/nebula/cipherquest/service/RecaptchaService.kt
@@ -1,6 +1,7 @@
 package code.nebula.cipherquest.service
 
 import code.nebula.cipherquest.models.dto.RecaptchaResponse
+import code.nebula.cipherquest.models.dto.RecaptchaVersion
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -12,26 +13,24 @@ import org.springframework.web.client.RestTemplate
 
 @Service
 class RecaptchaService(
-    @Value("\${recaptcha.secret-key:dummy-secret}") private val secretKey: String,
+    @Value("\${recaptcha.v3.secret-key:dummy-v3-secret}") private val v3SecretKey: String,
+    @Value("\${recaptcha.v2.secret-key:dummy-v2-secret}") private val v2SecretKey: String,
     @Value("\${recaptcha.verify-url:https://www.google.com/recaptcha/api/siteverify}")
     private val verifyUrl: String,
 ) {
-    private val restTemplate: RestTemplate = RestTemplate()
+    private val restTemplate = RestTemplate()
 
-    fun validateToken(recaptchaToken: String): RecaptchaResponse? {
-        val headers =
-            HttpHeaders().apply {
-                contentType = MediaType.APPLICATION_FORM_URLENCODED
-            }
-
+    fun validateToken(
+        recaptchaToken: String,
+        version: RecaptchaVersion,
+    ): RecaptchaResponse? {
+        val headers = HttpHeaders().apply { contentType = MediaType.APPLICATION_FORM_URLENCODED }
         val body =
             LinkedMultiValueMap<String, String>().apply {
-                add("secret", secretKey)
+                add("secret", if (version == RecaptchaVersion.V3) v3SecretKey else v2SecretKey)
                 add("response", recaptchaToken)
             }
-
         val requestEntity = HttpEntity(body, headers)
-
         val response =
             restTemplate.exchange(
                 verifyUrl,
