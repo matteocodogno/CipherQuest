c1c47504770c407cbbb6541474e14b6e40ed1d60
Giovanni Barbieri <giovanni.barbieri004@gmail.com>
2025-12-04 14:32:26 +0100

    fix: fixed bug with FixedBotMessages

--- backend/src/test/kotlin/code/nebula/cipherquest/controller/FixedBotMessageControllerTest.kt
+++ backend/src/test/kotlin/code/nebula/cipherquest/controller/FixedBotMessageControllerTest.kt
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.FilterType
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
